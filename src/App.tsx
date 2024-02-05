4e8e3b5b2998f7eb892b89b59c90660f342823f9
Lucas Evan <lucasevan478@gmail.com>
2024-02-05 12:17:03 -0300

    modificação do imc

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,64 @@
-import { useState } from "react"
-function App() {
-  const [altura, setAltura] = useState('');
-  const [peso, setPeso] = useState('');
-  const [resultado, seResultado] = useState('');
+import { useState } from "react";
+import './App.css';
 
-const calculaImc = () => {
-  const imc = altura / (peso * altura);
-  const fomrmatImc = imc.toFixed(2)
-  seResultado(+fomrmatImc)
-}
+function App() {
+  const [altura, setAltura] = useState(0);
+  const [peso, setPeso] = useState(0);
+  const [resultado, setResultado] = useState(0);
+  const [classificacao, setClassificacao] = useState("");
+
+  const calculaImc = () => {
+    const imc = peso / (altura * altura);
+    const imcFormatado = imc.toFixed(2);
+    let classificacaoImc;
+
+    if (imc < 18.5) {
+      classificacaoImc = "Abaixo do peso";
+    } else if (imc >= 18.5 && imc < 24.9) {
+      classificacaoImc = "Peso normal";
+    } else if (imc >= 25 && imc < 29.9) {
+      classificacaoImc = "Sobrepeso";
+    } else if (imc >= 30 && imc < 34.9) {
+      classificacaoImc = "Obesidade grau I";
+    } else if (imc >= 35 && imc < 39.9) {
+      classificacaoImc = "Obesidade grau II";
+    } else {
+      classificacaoImc = "Obesidade grau III";
+    }
+
+    setResultado(imcFormatado);
+    setClassificacao(classificacaoImc);
+  };
+
   return (
     <>
-     <div className="container">
-     <div className="App">
+      <div className="container">
         <h1>Calculadora de IMC</h1>
-        <div>
-          <label>Altura (cm): </label>
-          <input type="number"  onChange={(e) => setAltura(+e.target.value)} />
+        <div className="App">
+          <div>
+            <label>Altura (cm): </label>
+            <input
+              type="number"
+              onChange={(e) => setAltura(parseFloat(e.target.value) / 100)}
+            />
+          </div>
+          <div>
+            <label>Peso (kg): </label>
+            <input
+              type="number"
+              onChange={(e) => setPeso(parseFloat(e.target.value))}
+            />
+          </div>
+          <button onClick={calculaImc}>Calcular IMC</button>
         </div>
-        <div>
-          <label>Peso (kg): </label>
-          <input type="number"  onChange={(e) => setPeso(+e.target.value)} />
+        <div className="resultado">
+          <p>IMC: {resultado}</p>
+          <p>Classificação: {classificacao}</p>
         </div>
-        <button onClick={calculaImc}>Calcular IMC</button>
       </div>
-      {resultado}
-     </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
